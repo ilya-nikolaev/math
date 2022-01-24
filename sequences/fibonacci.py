d9ccb9840c20c400099d61dcbab1242413422bed
Ilya Nikolaev <ilyanikolaev28@inbox.ru>
2022-01-25 01:59:03 +0300

    🔥 Remove useless code

--- sequences/fibonacci.py
+++ sequences/fibonacci.py
@@ -1,23 +1,15 @@
-def recursive_fib(n: int):
-    if n < 0: raise ValueError("the number must be non-negative")
-    elif n == 0: return 0
-    elif n == 1: return 1
-    else: return recursive_fib(n - 1) + recursive_fib(n - 2)
-
-
-def fib(n: int):
+def fibonacci_sequence(n: int) -> list[int]:
     if n < 0: raise ValueError("the number must be non-negative")
     
-    lst = [0, 1]
-    
+    seq = [0, 1]
     for i in range(n - 1):
-        lst.append(lst[-1] + lst[-2])
+        seq.append(seq[-1] + seq[-2])
     
-    return lst[n]
+    return seq
 
 
 def main():
-    print(fib(int(input("Number: "))))
+    print(*fibonacci_sequence(10))
 
 
 if __name__ == '__main__':
